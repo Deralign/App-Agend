af0a22c66551a72bd45f777ba34b2feb750a7acb
Deralign <avysilvasan@gmail.com>
2025-04-24 21:47:35 -0400

    implementacao tela add bonita

---
+++ src/components/TarefaItem.js
@@ -0,0 +1,62 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function TarefaItem(props) {
+
+    if (props.status == 'concluído') {
+        statusColor = 'green';
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>{props.nome}</Text>
+            <Text style={styles.categoria}>{props.categoria}</Text>
+            <Text style={styles.data}>{props.data}</Text>
+            <View style={styles.status}>
+                <Text style={styles.textoStatus}>{props.status}</Text>
+            </View>
+        </View>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 130,
+        padding: 15,
+        borderBottomWidth: 1,
+        borderStyle: 'dashed',
+        borderColor: '#ccc'
+    },
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 21,
+        textAlign: 'center',
+    },
+    data: {
+        marginTop: 25,
+        marginLeft: 3.5,
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    categoria: {
+        marginTop: 8,
+        fontSize: 17,
+        marginLeft: 4
+    },
+    status: {
+        backgroundColor: 'orange',
+        width: 110,
+        height: 30,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        left: 305,
+        bottom: 10
+    },
+    textoStatus: {
+        color: 'white',
+        margin: 'auto',
+    }
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,104 @@
-import { View, Text} from 'react-native';
-
+import { View, Text, StyleSheet, TouchableOpacity, useState, ScrollView } from 'react-native';
+import TarefaItem from '../components/TarefaItem';
 export default function Home(){
+
+    
+
     return (
-        <View>
-            <Text>Home Page</Text>
+        <View style={styles.container}>
+            <View style={styles.cabecalho}>
+                <Text style={styles.titulo}>ABRIL/2025</Text>
+                <View style={styles.icone}>
+                    <Text style={styles.botaoFechar}>⚙️</Text>
+                </View>
+            </View>
+            <ScrollView style={styles.body}>
+                <TarefaItem 
+                nome="Tarefa 2"
+                status="a cumprir"
+                data="24/04/2004"
+                categoria="estudo"
+                />
+
+                <TarefaItem 
+                nome="Tarefa 2"
+                status="a cumprir"
+                data="24/04/2004"
+                categoria="estudo"
+                />
+
+                <TarefaItem 
+                nome="Tarefa 2"
+                status="a cumprir"
+                data="24/04/2004"
+                categoria="estudo"
+                />
+            </ScrollView>
+            <TouchableOpacity
+                style={styles.botaoAdicionar}
+                onPress={() => (
+                    alert("addedado")
+                )}
+                >
+                    <Text style={styles.emoji}>➕</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    cabecalho: {
+        backgroundColor: '#f1c739',
+        width: '100%',
+        height: 70,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomWidth: 1.5,
+        borderColor: 'black',
+        borderStyle: 'dashed',
+    },
+    titulo: {
+        color: 'white',
+        fontSize: 20,
+        marginTop: 2,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    icone: {
+        backgroundColor: 'grey',
+        width: 45,
+        height: 45,
+        borderRadius: 25,
+        right: 15,
+        position: 'absolute'
+    },
+    body: {
+        
+    },
+    botaoAdicionar: {
+        backgroundColor: "#ffe033",
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        position: 'absolute',
+        bottom: "2%",
+        right: "4%",
+        borderWidth: 3,
+        borderStyle: 'dashed',
+    },
+    emoji: {
+        margin: 'auto',
+        marginBottom: 8,
+        fontSize: 40,
+        color: 'white'
+    },
+    botaoFechar: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: 1.5,
+    },
+});
